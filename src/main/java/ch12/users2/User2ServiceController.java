75bfff830d8ad16d2766a27a0e0eb0bd12ee325e
choihyerln <choi.hyehyehihi@gmail.com>
2022-12-15 11:49:37 +0900

    JSP

--- src/main/java/ch12/users2/User2ServiceController.java
+++ src/main/java/ch12/users2/User2ServiceController.java
@@ -1,4 +1,4 @@
-package ch09.users;
+package ch12.users2;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -17,9 +17,9 @@ import org.mindrot.jbcrypt.BCrypt;
 /**
  * Servlet implementation class UserServiceController
  */
-@WebServlet({ "/ch09/users/list", "/ch09/users/login", "/ch09/users/logout", 
-			"/ch09/users/register", "/ch09/users/update", "/ch09/users/delete"})
-public class UserServiceController extends HttpServlet {
+@WebServlet({ "/ch12/users2/list", "/ch12/users2/login", "/ch12/users2/logout", 
+			"/ch12/users2/register", "/ch12/users2/update", "/ch12/users2/delete"})
+public class User2ServiceController extends HttpServlet {
 	
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
@@ -37,7 +37,7 @@ public class UserServiceController extends HttpServlet {
 		case "list":
 			List<User> list = dao.listUsers();
 			request.setAttribute("userList", list);
-			rd = request.getRequestDispatcher("/ch09/users/listView");
+			rd = request.getRequestDispatcher("/ch12/users2/list.jsp");
 			rd.forward(request, response);
 			break;
 		case "login":			
@@ -56,8 +56,8 @@ public class UserServiceController extends HttpServlet {
 //					out.print("location.href = '" + "/ch09/users/list" + "';");
 //					out.print("</script>");
 					request.setAttribute("msg", u.getUname() + "님 환영합니다.");
-					request.setAttribute("url", "/ch09/users/list");
-					rd = request.getRequestDispatcher("/ch09/users/alertMsg.jsp");
+					request.setAttribute("url", "/ch12/users2/list");
+					rd = request.getRequestDispatcher("/ch12/users2/alertMsg.jsp");
 					rd.forward(request, response);
 					
 					
@@ -68,8 +68,8 @@ public class UserServiceController extends HttpServlet {
 //					out.print("location.href = '" + "/ch09/users/login.html" + "';");
 //					out.print("</script>");
 					request.setAttribute("msg", "잘못된 패스워드입니다. 다시 입력하세요.");
-					request.setAttribute("url", "/ch09/users/login.html");
-					rd = request.getRequestDispatcher("/ch09/users/alertMsg.jsp");
+					request.setAttribute("url", "/ch12/users2/login.html");
+					rd = request.getRequestDispatcher("/ch12/users2/alertMsg.jsp");
 					rd.forward(request, response);
 				}
 			}
@@ -82,31 +82,39 @@ public class UserServiceController extends HttpServlet {
 //				out.print("location.href = '" + "/ch09/users/register.html" + "';");
 //				out.print("</script>");
 				request.setAttribute("msg", "회원 가입 페이지로 이동합니다.");
-				request.setAttribute("url", "/ch09/users/register.html");
-				rd = request.getRequestDispatcher("/ch09/users/alertMsg.jsp");
+				request.setAttribute("url", "/ch12/users2/register");
+				rd = request.getRequestDispatcher("/ch12/users2/alertMsg.jsp");
 				rd.forward(request, response);
 			}
 			break;
 		case "logout":
 			session.invalidate();
-			response.sendRedirect("/ch09/users/list");
+			response.sendRedirect("/ch12/users2/list");
 			break;
 		case "register":
-			uid = request.getParameter("uid");
-			pwd = request.getParameter("pwd");
-			pwd2 = request.getParameter("pwd2");
-			uname = request.getParameter("uname");
-			email = request.getParameter("email");
-			if (pwd.equals(pwd2)) {
-				u = new User(uid, pwd, uname, email);
-				dao.resisterUser(u);
-				response.sendRedirect("/ch09/users/list");
-			}
-			else {
-				out.print("<script>");
-				out.print("alert('동일한 패스워드를 입력하세요.');");
-				out.print("location.href='" + "/ch09/users/register.html" + "';");
-				out.print("</script>");
+			if (request.getMethod().equals("GET")) {
+				response.sendRedirect("/ch12/users2/register");
+			} else {
+				uid = request.getParameter("uid");
+				pwd = request.getParameter("pwd");
+				pwd2 = request.getParameter("pwd2");
+				uname = request.getParameter("uname");
+				email = request.getParameter("email");
+				if (pwd.equals(pwd2)) {
+					u = new User(uid, pwd, uname, email);
+					dao.resisterUser(u);
+					response.sendRedirect("/ch12/users2/list");
+				}
+				else {
+					request.setAttribute("msg", "동일한 패스워드를 입력하세요.");
+					request.setAttribute("url", "/ch12/users2/register");
+					rd = request.getRequestDispatcher("/ch12/users2/alertMsg.jsp");
+					rd.forward(request, response);
+//					out.print("<script>");
+//					out.print("alert('동일한 패스워드를 입력하세요.');");
+//					out.print("location.href='" + "/ch12/users2/register.html" + "';");
+//					out.print("</script>");
+				}
 			}
 			break;
 		case "update":
@@ -114,7 +122,7 @@ public class UserServiceController extends HttpServlet {
 				uid = request.getParameter("uid");
 				u = dao.getUserInfo(uid);
 				request.setAttribute("user", u);
-				rd = request.getRequestDispatcher("/ch09/users/updateView");
+				rd = request.getRequestDispatcher("/ch12/users2/update.jsp");
 				rd.forward(request, response);
 			}
 			else {
@@ -123,13 +131,14 @@ public class UserServiceController extends HttpServlet {
 				email = request.getParameter("email");
 				u = new User(uid, uname, email);
 				dao.updateUser(u);
-				response.sendRedirect("/ch09/users/list");
+				session.setAttribute("uname", uname);
+				response.sendRedirect("/ch12/users2/list");
 			}
 			break;
 		case "delete":
 			uid = request.getParameter("uid");
 			dao.deleteUser(uid);
-			response.sendRedirect("/ch09/users/list");
+			response.sendRedirect("/ch12/users2/list");
 			break;
 		default:
 			System.out.println(request.getMethod() + " 잘못된 경로");
